82ccd0b14914a694f98906365c5325fb877ff198
cW4euY <cW4euY@cyber-dojo.org>
2024-06-19 07:28:45 +0000

    15 ran tests, predicted green, got green

--- files/main.go
+++ files/main.go
@@ -1,30 +1,27 @@
 package common_prefix
 
-func Longest(words []string) (result int) {
+func Longest(words []string) int {
     size := len(words)
     
     if size == 0 {
-        return
+        return 0
     }
     
     if size == 1 {
         return len(words[0])
     }
     
-    for {
-        index := result + 1
+    for l := 0;; l++ {
+        index := l + 1
         if index > len(words[0]) {
-            return
+            return l
         }
         
         prefix := words[0][:index]
         for i := 1; i < size; i++ {
             if index > len(words[i]) || words[i][:index] != prefix {
-                return
+                return l
             }
         }
-        result++
     }
-    
-    return
 }

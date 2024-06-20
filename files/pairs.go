60758575ad753a2e7c3812d08c096990e43026a5
cW4euY <cW4euY@cyber-dojo.org>
2024-06-20 06:57:29 +0000

    32 ran tests, predicted green, got red

--- files/pairs.go
+++ files/pairs.go
@@ -1,5 +1,19 @@
 package common_prefix
 
 func allPairs(words []string) (result [][]string) {
+    size := len(words)
+    
+    if size < 2 {
+        return
+    }
+    
+    for i := 0; i < size; i++ {
+        for j := 0; j < size; j++ {
+            if i != j {
+                result = append(result, []string{words[i], words[j]})
+            }
+        }    
+    }    
+    
     return
 }
